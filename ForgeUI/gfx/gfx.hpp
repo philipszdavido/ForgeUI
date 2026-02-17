5fb8bcd66485eadfd5c417872fb56f7bbc3d8c0b
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-02-17 07:22:20 +0100

    feat: defined methods for clearing screen, drawing rectangles, characters, and lines

--- ForgeUI/gfx/gfx.hpp
+++ ForgeUI/gfx/gfx.hpp
@@ -9,5 +9,17 @@
 #define gfx_hpp
 
 #include <stdio.h>
+#include <stdint.h>
+
+#define WIDTH  800
+#define HEIGHT 600
+
+extern uint32_t framebuffer[WIDTH * HEIGHT];
+
+void clear(uint32_t color);
+void put_pixel(int x, int y, uint32_t color);
+void draw_rect(int x, int y, int w, int h, uint32_t color);
+void draw_line(int x0, int y0, int x1, int y1, uint32_t color);
+void draw_char(int x, int y, char c, uint32_t color);
 
 #endif /* gfx_hpp */
